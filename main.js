01566a94a20f9c6ba5adbafd6447562409bfb583
Glenn McCrea <glennmccrea@icloud.com>
2025-09-27 17:15:13 -0500

    Working add items!

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-const addedItem =
+const myForm = document.getElementById("list-form");
+const itemList = document.getElementById("items-list");
+
+myForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let textInput = document.getElementById("text-input").value;
+  console.log(textInput);
+  const addItem = function (textInput) {
+    if (textInput !== "") {
+      itemList.innerHTML += `<li>${textInput}</li>`;
+    }
+  };
+  return addItem(textInput);
+});
